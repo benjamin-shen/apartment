78e6980c47e0550260a84aa1e30a78fa7f1669d0
Benjamin Shen <bfs45@cornell.edu>
2020-08-16 00:10:32 -0400

    Add local throttle to doorbell and login functions

--- src/components/Doorbell.js
+++ src/components/Doorbell.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext, useRef } from "react";
 import { Button } from "react-bootstrap";
 import useSound from "use-sound";
+import { useLocalStorage, writeStorage } from "@rehooks/local-storage";
 import moment from "moment";
 import "moment-timezone";
 import app from "./base";
@@ -8,6 +9,15 @@ import { AuthContext } from "./Auth";
 import dingdong from "../assets/sounds/dingdong.mp3";
 
 moment.tz.setDefault("Americas/New_York");
+const isValidDate = (str) => {
+  var d = moment(str);
+  if (d == null || !d.isValid()) return false;
+  return str.indexOf(d.format() >= 0);
+};
+
+const appName = require("../env.json").app.name;
+const lastInvokedKey = `${appName}-lastInvokedDoorbell`;
+const lastInvokedThrottle = 60000;
 
 const log = app.firestore().collection("log");
 const ringDoorbell = app.functions().httpsCallable("ringDoorbell");
@@ -17,55 +27,75 @@ function Doorbell() {
   const [playSound] = useSound(dingdong);
   const [rang, setRang] = useState(false);
   const [justRang, setJustRang] = useState(false);
-  const mountedRef = useRef(true);
 
+  const mountedRef = useRef(true);
   useEffect(() => {
     return () => {
       mountedRef.current = false;
     };
   }, []);
 
+  const [lastInvoked] = useLocalStorage(lastInvokedKey);
+
   const { currentUser } = useContext(AuthContext);
 
   const ring = () => {
+    console.log("Rang doorbell.");
     const time = moment();
     const month = time.format("YYYY-MM");
 
-    // TODO add checks to avoid spam
-    ringDoorbell()
-      .then(() => {
-        console.log("Rang doorbell.");
-      })
-      .catch((err) => {
-        console.log("Couldn't ring doorbell.");
-        console.log(err);
-      });
+    try {
+      if (lastInvoked && !isValidDate(lastInvoked)) {
+        console.log("Local storage corrupted.");
+        console.log(lastInvokedKey + ": " + lastInvoked);
+        writeStorage(lastInvokedKey, time.format());
+      }
+      if (
+        !lastInvoked ||
+        time.diff(moment(lastInvoked), "minutes") >= lastInvokedThrottle
+      ) {
+        writeStorage(lastInvokedKey, time.format());
 
-    if (currentUser && currentUser.emailVerified) {
-      log
-        .doc(month)
-        .collection("doorbell")
-        .add({ user: currentUser.email, time: new Date(time) })
-        .then(() => {
-          console.log("Logged doorbell.");
-        })
-        .catch((err) => {
-          console.log("Couldn't log doorbell.");
-          console.log(err);
-        });
-    } else {
-      logDoorbell(month)
-        .then(() => {
-          if (currentUser) {
-            console.log("Logged unverified doorbell.");
-          } else {
-            console.log("Logged anonymous doorbell.");
-          }
-        })
-        .catch((err) => {
-          console.log("Couldn't log doorbell.");
-          console.log(err);
-        });
+        ringDoorbell()
+          .then(() => {
+            console.log("Notified residents.");
+          })
+          .catch((err) => {
+            console.log("Couldn't notify residents.");
+            console.log(err);
+          });
+
+        if (currentUser && currentUser.emailVerified) {
+          log
+            .doc(month)
+            .collection("doorbell")
+            .add({ user: currentUser.email, time: new Date(time) })
+            .then(() => {
+              console.log("Logged doorbell.");
+            })
+            .catch((err) => {
+              console.log("Couldn't log doorbell.");
+              console.log(err);
+            });
+        } else {
+          logDoorbell(month)
+            .then(() => {
+              if (currentUser) {
+                console.log("Logged unverified doorbell.");
+              } else {
+                console.log("Logged anonymous doorbell.");
+              }
+            })
+            .catch((err) => {
+              console.log("Couldn't log doorbell.");
+              console.log(err);
+            });
+        }
+      }
+    } catch (err) {
+      console.log("Local storage error.");
+      console.log(err);
+      writeStorage(lastInvokedKey, time.format());
     }
 
     setRang(true);
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,18 +1,30 @@
 import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { AuthContext } from "./Auth";
+import { useLocalStorage, writeStorage } from "@rehooks/local-storage";
 import moment from "moment";
 import "moment-timezone";
 import app from "./base";
+import { AuthContext } from "./Auth";
 import User from "./User";
 import Guest from "./Guest";
 
 moment.tz.setDefault("Americas/New_York");
+const isValidDate = (str) => {
+  var d = moment(str);
+  if (d == null || !d.isValid()) return false;
+  return str.indexOf(d.format() >= 0);
+};
+
+const appName = require("../env.json").app.name;
+const lastInvokedKey = `${appName}-lastInvokedLogin`;
+const lastInvokedThrottle = 3600000;
 
 const log = app.firestore().collection("log");
 
 const PrivateRoute = ({ component, guest, ...rest }) => {
   const { currentUser, guestUser } = useContext(AuthContext);
+
+  const [lastInvoked] = useLocalStorage(lastInvokedKey);
 
   if (!currentUser) {
     return <Redirect to={guest ? "/login/guest" : "/login/user"} />;
@@ -25,17 +37,36 @@ const PrivateRoute = ({ component, guest, ...rest }) => {
   const logLogin = (user) => {
     if (user) {
       const time = moment();
-      log
-        .doc(time.format("YYYY-MM"))
-        .collection("login")
-        .add({ user, time: new Date(time) })
-        .then(() => {
-          console.log("Logged login.");
-        })
-        .catch((err) => {
-          console.log("Couldn't log login.");
-          console.log(err);
-        });
+
+      try {
+        if (lastInvoked && !isValidDate(lastInvoked)) {
+          console.log("Local storage corrupted.");
+          console.log(lastInvokedKey + ": " + lastInvoked);
+          writeStorage(lastInvokedKey, time.format());
+        }
+        if (
+          !lastInvoked ||
+          time.diff(moment(lastInvoked)) >= lastInvokedThrottle
+        ) {
+          writeStorage(lastInvokedKey, time.format());
+
+          log
+            .doc(time.format("YYYY-MM"))
+            .collection("login")
+            .add({ user, time: new Date(time) })
+            .then(() => {
+              console.log("Logged login.");
+            })
+            .catch((err) => {
+              console.log("Couldn't log login.");
+              console.log(err);
+            });
+        }
+      } catch (err) {
+        console.log("Local storage error.");
+        console.log(err);
+        writeStorage(lastInvokedKey, time.format());
+      }
     }
   };
 
